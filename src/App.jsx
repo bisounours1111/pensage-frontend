b1d35bfd2af122cbb9746bc14daa6e3fc96a8d0a
Kob0o <engerardot@gmail.com>
2025-10-27 15:16:04 +0100

    feat: Création de la page signup + welcomePage template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MyStoriesPage from './pages/stories/MyStoriesPage'
+import SignupPage from './pages/auth/SignupPage'
+import WelcomePage from './pages/auth/WelcomePage'
 import './App.css'
 
 function App() {
@@ -7,9 +9,12 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Routes>
-          {/* Routes principales */}
-          <Route path="/signup" element={<div>Signup</div>} />
+          {/* Routes d'authentification */}
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<div>Login</div>} />
+          
+          {/* Routes principales */}
           <Route path="/home" element={<div>Home</div>} />
           <Route path="/stories" element={<MyStoriesPage />} />
           <Route path="/create" element={<div>Éditeur</div>} />
@@ -17,9 +22,6 @@ function App() {
           <Route path="/profile" element={<div>Profil</div>} />
           <Route path="/community" element={<div>Communauté</div>} />
           <Route path="/publish" element={<div>Publication</div>} />
-
-          {/* Route par défaut */}
-          <Route path="/" element={<div>Accueil</div>} />
         </Routes>
       </div>
     </Router>
